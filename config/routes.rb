e739f790f0203473e875a8ac93dd0ea53652b3cb
Amolterer <alexander.molterer@kellogg.northwestern.edu>
2022-06-07 16:28:34 +0000

    S1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
   get "/", :controller => "places", :action => "index"
   resources "places"
   resources "posts"
